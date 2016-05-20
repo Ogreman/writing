f119a20ef51343f9d0f59a9e21185cd9738a6c28
Jamie Cox <jamie@potatolondon.com>
2016-05-20 13:08:01 +0100

    titles

--- writing/models.py
+++ writing/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
-# Create your models here.
 class Writing(models.Model):
     content = models.TextField()
-    
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
