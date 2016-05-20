3f3450639a2aef2cec8ad4c75e328d70b06c4fd0
Jamie Cox <jamie@potatolondon.com>
2016-05-20 13:21:42 +0100

    things

--- writing/models.py
+++ writing/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Writing(models.Model):
     content = models.TextField()
     title = models.CharField(max_length=255)
+    active = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
--- writing/views.py
+++ writing/views.py
@@ -4,11 +4,19 @@ from django.views.generic import ListView, DetailView
 from .models import Writing
 
 
-class WritingListView(ListView):
+class ActiveObjectMixin(object):
+
+    def get_queryset(self):
+        queryset = super(ActiveObjectMixin, self).get_queryset()
+        queryset = queryset.filter(active=True)
+        return queryset
+
+
+class WritingListView(ActiveObjectMixin, ListView):
     model = Writing
     template_name = "list.html"
 
 
-class WritingDetailView(DetailView):
+class WritingDetailView(ActiveObjectMixin, DetailView):
     model = Writing
     template_name = "text.html"
