20d464ae0824668450e1e6efdedf89658cdf6d40
Jamie Cox <jamie@potatolondon.com>
2016-05-22 10:12:17 +0100

    display

--- writing/admin.py
+++ writing/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Writing, View
 
-admin.site.register(Writing)
+class WritingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'num_views',)
+
+    def num_views(self, instance):
+        return instance.view_set.count()
+
+admin.site.register(Writing, WritingAdmin)
 admin.site.register(View)
